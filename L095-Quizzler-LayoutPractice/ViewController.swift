2b4681cc316c24f728427fb3f4464c5d18093f7a
ArthurNikitin <Arthur.V.Nikitin@gmail.com>
2022-09-12 11:07:19 +0300

    97 finish

--- L095-Quizzler-LayoutPractice/ViewController.swift
+++ L095-Quizzler-LayoutPractice/ViewController.swift
@@ -10,11 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        ["2 + 3 = ?", "True"],
+        ["4 + 5 = ?", "True"],
+        ["5 - 2 = ?", "False"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //questionLabel.self.text = "23"
+        updateUI()
+        
+        
+        //trueButton.setTitle("5", for: .normal)
+        
+        //falseButton.setTitle("3", for: .normal)
+        
+        
     }
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer   {
+            print("Right!")
+        }
+        else    {
+            print("Wrong!")
+        }
+        
+        if quiz.count > questionNumber + 1 {
+            questionNumber += 1
+        }  else {
+            questionNumber = 0
+            print("Finish")
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    
 
 }
 
